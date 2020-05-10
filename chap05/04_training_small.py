02342629d2614b2d76212cb42da1eb34960822f9
ma-xiaodong <xdma@163.com>
2020-05-10 22:26:15 +0800

    train and inference use small cats and dogs dataset

--- chap05/04_training_small.py
+++ chap05/04_training_small.py
@@ -1,10 +1,12 @@
-import os, shutil
+import os, sys, shutil
 from keras import layers
 from keras import models
 from keras import optimizers
+from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
-TRAIN_SZ=2000
+# copy maximum number of pics to destination dir
+MAX_PICS=4000
 VAL_SZ=500
 TEST_SZ=500
 BATCH_SZ=20
@@ -29,7 +31,7 @@ def copy_pics(original_cats_dir, original_dogs_dir,
     os.mkdir(test_cats_dir)
     os.mkdir(test_dogs_dir)
 
-    fnames = ['{}.jpg'.format(i) for i in range(TRAIN_SZ)]
+    fnames = ['{}.jpg'.format(i) for i in range(MAX_PICS)]
     # copy train pics
     for fname in fnames:
         # copy cats
@@ -43,7 +45,7 @@ def copy_pics(original_cats_dir, original_dogs_dir,
         shutil.copyfile(src, dst)
 
     # copy validation pics
-    fnames = ['{}.jpg'.format(i) for i in range(TRAIN_SZ, TRAIN_SZ+VAL_SZ)]
+    fnames = ['{}.jpg'.format(i) for i in range(MAX_PICS, MAX_PICS+VAL_SZ)]
     for fname in fnames:
         # copy cats
         src = os.path.join(original_cats_dir, fname)
@@ -57,7 +59,7 @@ def copy_pics(original_cats_dir, original_dogs_dir,
 
     # copy test pics
     fnames = ['{}.jpg'.format(i) 
-               for i in range(TRAIN_SZ+VAL_SZ, TRAIN_SZ+VAL_SZ+TEST_SZ)]
+               for i in range(MAX_PICS+VAL_SZ, MAX_PICS+VAL_SZ+TEST_SZ)]
     for fname in fnames:
         # copy cats
         src = os.path.join(original_cats_dir, fname)
@@ -69,7 +71,7 @@ def copy_pics(original_cats_dir, original_dogs_dir,
         dst = os.path.join(test_dogs_dir, fname)
         shutil.copyfile(src, dst)
 
-def training(train_dir, validation_dir, test_dir):
+def training(train_sz, train_dir, validation_dir, test_dir):
     model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation='relu', 
                             input_shape=(150, 150, 3)))
@@ -81,6 +83,7 @@ def training(train_dir, validation_dir, test_dir):
     model.add(layers.Conv2D(128, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D(2, 2))
     model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(512, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
@@ -105,10 +108,29 @@ def training(train_dir, validation_dir, test_dir):
 
     history = model.fit_generator(
             train_generator,
-            steps_per_epoch=TRAIN_SZ*2//BATCH_SZ,
+            steps_per_epoch=train_sz*2//BATCH_SZ,
             epochs=EPOCHS,
             validation_data=validation_generator,
             validation_steps=VAL_SZ*2//BATCH_SZ)
+
+    model.save('cats_and_dogs_'+str(train_sz)+'.h5')
+
+def test(train_sz, test_dir):
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_generator = test_datagen.flow_from_directory(
+            test_dir,
+            target_size=(150, 150),
+            batch_size=BATCH_SZ,
+            class_mode='binary')
+
+    base_dir = './models'
+    model_path = os.path.join(base_dir, 'cats_and_dogs_'+str(train_sz)+'.h5')
+    model = load_model(model_path)
+
+    test_loss, test_acc = model.evaluate_generator(test_generator, 
+                                                   steps=TEST_SZ*2/BATCH_SZ)
+    print('======== test loss and accuracy ========')
+    print('%.4f, %.4f'%(test_loss, test_acc))
 
 if __name__ == '__main__':
     original_cats_dir = '/media/mxd/Document/data/kagglecatsanddogs/PetImages/Cat'
@@ -126,4 +148,15 @@ if __name__ == '__main__':
         copy_pics(original_cats_dir, original_dogs_dir, 
                   train_dir, validation_dir, test_dir)
 
-    training(train_dir, validation_dir, test_dir)
+    if len(sys.argv) != 3:
+        print('Usage: [train training_numbers] or [test training_numbers]')
+        sys.exit(0)
+
+    train_sz = int(sys.argv[2])
+    if sys.argv[1] == 'train':
+        training(train_sz, train_dir, validation_dir, test_dir)
+    elif sys.argv[1] == 'test':
+        test(train_sz, test_dir)
+    else:
+        print('Unknown use mode!')
+
